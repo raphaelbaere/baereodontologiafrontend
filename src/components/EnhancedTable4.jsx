cfb12c1f9ef367dd6735ceb9214e63ce5a04b604
Raphael Baere de Sá Oliveira <raphaelbaere@id.uff.br>
2023-05-10 14:35:50 -0300

    Update EnhancedTable4.jsx

--- src/components/EnhancedTable4.jsx
+++ src/components/EnhancedTable4.jsx
@@ -338,6 +338,7 @@ export default function EnhancedTable4(props) {
       const rows = await createRows4();
       setTreatmentRows(rows);
       setTreatmentRowsOG(rows);
+      console.log(rows)
       const filteredTreatmentRows = rows.filter((treatmentRows) => treatmentRows.realizado === 'Sim');
       setTreatmentRows(filteredTreatmentRows);
       let rowsOnMount = stableSort(
