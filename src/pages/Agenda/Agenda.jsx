738a9bcc2b1fdb04ee0ba9a5e7ea7550015cc5d2
raphaelbaere <raphaelbaere@id.uff.br>
2024-02-20 20:38:29 -0300

    fixsaxsx

--- src/pages/Agenda/Agenda.jsx
+++ src/pages/Agenda/Agenda.jsx
@@ -12,6 +12,41 @@ moment.locale('pt-br');
 
 const localizer = momentLocalizer(moment);
 
+const monthMap = {
+    'Jan': '01',
+    'Feb': '02',
+    'Mar': '03',
+    'Apr': '04',
+    'May': '05',
+    'Jun': '06',
+    'Jul': '07',
+    'Aug': '08',
+    'Sep': '09',
+    'Oct': '10',
+    'Nov': '11',
+    'Dec': '12'
+};
+
+function convertToISODateString(dateString) {
+    // Dividir a string em partes usando o espaço como separador entre data e hora
+    const parts = dateString.split(' ');
+
+    // Dividir a parte da data em partes usando '/' como separador
+    const lengthPart4 = parts[4].length;
+    let horaAUsar = parts[4];
+
+    if (lengthPart4 === 7) {
+        horaAUsar = '0' + horaAUsar;
+    }
+
+    // Reorganizar as partes para o formato ISO 8601
+    const isoDateString = `${parts[3]}-${monthMap[parts[1]]}-${parts[2]}T${horaAUsar}`;
+
+    console.log(isoDateString);
+
+    return isoDateString;
+}
+
 export default function Agenda(props, {
     dayLayoutAlgorithm = 'no-overlap',
 }) {
@@ -21,7 +56,7 @@ export default function Agenda(props, {
     const { urlRequisicao, handleOpen14, setEventSelected } = React.useContext(BaereContext);
 
     useEffect(() => {
-        
+
 
         const fetchEventos = async () => {
             const response = await fetch(`${urlRequisicao}/eventos`);
@@ -29,77 +64,44 @@ export default function Agenda(props, {
             const dataMapped = data.map((evento) => {
                 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
                 const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-                
-                const dateStringStart = evento.start_date;
-                const dateObjStart = new Date(dateStringStart);
-                
-                const dateStringEnd = evento.end_date;
-                const dateObjEnd = new Date(dateStringEnd);
+                console.log(evento, 'evento')
+                const dateObjStart = new Date(evento.start_date);
+                const dateObjEnd = new Date(evento.end_date);
 
-                const monthStart = months[dateObjStart.getUTCMonth() - 1];
-                const dayOfWeekStart = daysOfWeek[dateObjStart.getUTCDay() + 2];
-                let dayStart = dateObjStart.getUTCDate() + 17;
-                const yearStart = dateObjStart.getUTCFullYear();
-                let hourStart = ('0' + dateObjStart.getUTCHours()).slice(-2);
-                console.log(hourStart, 'start')
-                if (hourStart >= 15 && hourStart > 0) {
-                    hourStart = ('0' + dateObjStart.getUTCHours()).slice(-2) - 15;
-                } else if (hourStart == 0) {
-                    hourStart = (dateObjStart.getUTCHours() + 9)
-                    dayStart = dateObjStart.getUTCDate() + 16;
-                } else if (hourStart >= 12 && hourStart < 15) {
-                    hourStart = (dateObjStart.getUTCHours() + 9);
-                    dayStart = dateObjStart.getUTCDate() + 17;
-                } else if (hourStart > 0 && hourStart <= 15) {
-                    hourStart = (dateObjStart.getUTCHours() + 9);
-                    dayStart = dateObjStart.getUTCDate() + 16;
-                }
-                const minuteStart = ('0' + dateObjStart.getUTCMinutes()).slice(-2);
-                const secondStart = ('0' + dateObjStart.getUTCSeconds()).slice(-2);
+                const monthStart = months[dateObjStart.getMonth()];
+                const dayOfWeekStart = daysOfWeek[dateObjStart.getDay()];
+                const dayStart = dateObjStart.getDate();
+                const yearStart = dateObjStart.getFullYear();
+                const hourStart = dateObjStart.getHours() + 3;
+                const minuteStart = ('0' + dateObjStart.getMinutes()).slice(-2);
+                const secondStart = ('0' + dateObjStart.getSeconds()).slice(-2);
                 const timezoneOffsetStart = '-0300';
-                
-                const monthEnd = months[dateObjEnd.getUTCMonth() - 1];
-                const dayOfWeekEnd = daysOfWeek[dateObjEnd.getUTCDay() + 2];
-                let dayEnd = dateObjEnd.getUTCDate() + 17;
-                const yearEnd = dateObjEnd.getUTCFullYear();
-                let hourEnd = ('0' + dateObjEnd.getUTCHours()).slice(-2);
-                console.log(hourEnd, 'end');
-                if (hourEnd >= 15 && hourEnd > 0) {
-                    hourEnd = ('0' + dateObjEnd.getUTCHours()).slice(-2) - 15;
-                    console.log(hourEnd, 'end')
-                } else if (hourEnd == 0) {
-                    hourEnd = (dateObjEnd.getUTCHours() + 9);
-                    dayEnd = dateObjEnd.getUTCDate() + 16;
-                    console.log(hourEnd, 'end')
-                } else if (hourEnd >= 12 && hourEnd < 15) {
-                    hourEnd = (dateObjEnd.getUTCHours() + 9);
-                    dayEnd = dateObjEnd.getUTCDate() + 17;
-                } else if (hourEnd > 0 && hourEnd <= 15) {
-                    hourEnd = (dateObjEnd.getUTCHours() + 9);
-                    dayEnd = dateObjEnd.getUTCDate() + 16;
-                }
-                const minuteEnd = ('0' + dateObjEnd.getUTCMinutes()).slice(-2);
-                const secondEnd = ('0' + dateObjEnd.getUTCSeconds()).slice(-2);
+
+                const monthEnd = months[dateObjEnd.getMonth()];
+                const dayOfWeekEnd = daysOfWeek[dateObjEnd.getDay()];
+                const dayEnd = dateObjEnd.getDate();
+                const yearEnd = dateObjEnd.getFullYear();
+                const hourEnd = dateObjEnd.getHours() + 3;
+                const minuteEnd = ('0' + dateObjEnd.getMinutes()).slice(-2);
+                const secondEnd = ('0' + dateObjEnd.getSeconds()).slice(-2);
                 const timezoneOffsetEnd = '-0300';
-                
-                const formattedDateStart = `${dayOfWeekStart ? dayOfWeekStart : daysOfWeek[0]} ${monthStart} ${dayStart} ${yearStart} ${hourStart}:${minuteStart}:${secondStart} GMT${timezoneOffsetStart} (Horário Padrão de Brasília)`;
-                
-                const formattedDateEnd = `${dayOfWeekEnd ? dayOfWeekEnd : daysOfWeek[0]} ${monthEnd} ${dayEnd} ${yearEnd} ${hourEnd}:${minuteEnd}:${secondEnd} GMT${timezoneOffsetEnd} (Horário Padrão de Brasília)`;
-                
-                console.log(formattedDateStart);
-                console.log(formattedDateEnd);
+
+                const formattedDateStart = `${dayOfWeekStart} ${monthStart} ${dayStart} ${yearStart} ${hourStart}:${minuteStart}:${secondStart} GMT${timezoneOffsetStart >= 0 ? '+' : ''}${timezoneOffsetStart}:00 (Horário Padrão de Brasília)`;
+
+                const formattedDateEnd = `${dayOfWeekEnd} ${monthEnd} ${dayEnd} ${yearEnd} ${hourEnd}:${minuteEnd}:${secondEnd} GMT${timezoneOffsetEnd >= 0 ? '+' : ''}${timezoneOffsetEnd}:00 (Horário Padrão de Brasília)`;
+
                 return {
-            id: evento.id,
-            start: new Date(formattedDateStart),
-            end: new Date(formattedDateEnd),
-            title: evento.titulo
-        }
-        })
-        
-        console.log(data)
-        console.log(dataMapped);
-        setMyEvents(dataMapped);
-        setAtualize(Math.random() * 99999)
+                    id: evento.id,
+                    start: moment(convertToISODateString(formattedDateStart)).toDate(),
+                    end: moment(convertToISODateString(formattedDateEnd)).toDate(),
+                    title: evento.titulo
+                }
+            })
+
+            console.log(data)
+            console.log(dataMapped);
+            setMyEvents(dataMapped);
+            setAtualize(Math.random() * 99999)
         }
         fetchEventos();
         console.log(atualize2)
@@ -113,7 +115,7 @@ export default function Agenda(props, {
         },
         [setMyEvents]
     );
-    
+
 
     const { defaultDate, scrollToTime, messages } = useMemo(
         () => ({
@@ -147,40 +149,40 @@ export default function Agenda(props, {
             const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
             const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-                const dateStringStart = new Date(start).toISOString();
-                const dateObjStart = new Date(dateStringStart);
-                const dateStringEnd = new Date(end).toISOString();
-                const dateObjEnd = new Date(dateStringEnd);
-                
-                const monthStart = months[dateObjStart.getMonth()];
-                const dayOfWeekStart = daysOfWeek[dateObjStart.getDay()];
-                const dayStart = dateObjStart.getDate();
-                const yearStart = dateObjStart.getFullYear();
-                const hourStart = dateObjStart.getUTCHours();
-                const minuteStart = ('0' + dateObjStart.getUTCMinutes()).slice(-2);
-                const secondStart = ('0' + dateObjStart.getUTCSeconds()).slice(-2);
-                const timezoneOffsetStart = '-0300';
-        
-                const monthEnd = months[dateObjEnd.getMonth()];
-                const dayOfWeekEnd = daysOfWeek[dateObjEnd.getDay()];
-                const dayEnd = dateObjEnd.getDate();
-                const yearEnd = dateObjEnd.getFullYear();
-                const hourEnd = dateObjEnd.getUTCHours();
-                const minuteEnd = ('0' + dateObjEnd.getUTCMinutes()).slice(-2);
-                const secondEnd = ('0' + dateObjEnd.getUTCSeconds()).slice(-2);
-                const timezoneOffsetEnd = '-0300';
-        
-        
-                const formattedDateStart = `${dayOfWeekStart} ${monthStart} ${dayStart} ${yearStart} ${hourStart}:${minuteStart}:${secondStart
+            const dateStringStart = new Date(start).toISOString();
+            const dateObjStart = new Date(dateStringStart);
+            const dateStringEnd = new Date(end).toISOString();
+            const dateObjEnd = new Date(dateStringEnd);
+
+            const monthStart = months[dateObjStart.getMonth()];
+            const dayOfWeekStart = daysOfWeek[dateObjStart.getDay()];
+            const dayStart = dateObjStart.getDate();
+            const yearStart = dateObjStart.getFullYear();
+            const hourStart = dateObjStart.getUTCHours() - 3;
+            const minuteStart = ('0' + dateObjStart.getUTCMinutes()).slice(-2);
+            const secondStart = ('0' + dateObjStart.getUTCSeconds()).slice(-2);
+            const timezoneOffsetStart = '-0300';
+
+            const monthEnd = months[dateObjEnd.getMonth()];
+            const dayOfWeekEnd = daysOfWeek[dateObjEnd.getDay()];
+            const dayEnd = dateObjEnd.getDate();
+            const yearEnd = dateObjEnd.getFullYear();
+            const hourEnd = dateObjEnd.getUTCHours() - 3;
+            const minuteEnd = ('0' + dateObjEnd.getUTCMinutes()).slice(-2);
+            const secondEnd = ('0' + dateObjEnd.getUTCSeconds()).slice(-2);
+            const timezoneOffsetEnd = '-0300';
+
+
+            const formattedDateStart = `${dayOfWeekStart} ${monthStart} ${dayStart} ${yearStart} ${hourStart}:${minuteStart}:${secondStart
                 } GMT${timezoneOffsetStart >= 0 ? '+' : ''}${timezoneOffsetStart}:00 (Horário Padrão de Brasília)`;
-        
-                const formattedDateEnd = `${dayOfWeekEnd} ${monthEnd} ${dayEnd} ${yearEnd} ${hourEnd}:${minuteEnd}:${secondEnd
+
+            const formattedDateEnd = `${dayOfWeekEnd} ${monthEnd} ${dayEnd} ${yearEnd} ${hourEnd}:${minuteEnd}:${secondEnd
                 } GMT${timezoneOffsetEnd >= 0 ? '+' : ''}${timezoneOffsetEnd}:00 (Horário Padrão de Brasília)`;
 
-                console.log(formattedDateStart)
-                console.log(formattedDateEnd)
-            setStartDate(new Date(formattedDateStart));
-            setEndDate(new Date(formattedDateEnd));
+            console.log(formattedDateStart)
+            console.log(formattedDateEnd)
+            setStartDate(moment(convertToISODateString(formattedDateStart)).toDate());
+            setEndDate(moment(convertToISODateString(formattedDateEnd)).toDate());
             handleOpen10();
         },
         [myEvents]
@@ -190,7 +192,7 @@ export default function Agenda(props, {
         <Fragment>
             <BasicModal10 setAtualize={setAtualize} setAtualize2={setAtualize2} setEvents={setMyEvents} handleSelectSlot={handleSelectSlot} />
             <BasicModal14 setAtualize={setAtualize} setAtualize2={setAtualize2} setEvents={setMyEvents} handleSelectSlot={handleSelectSlot} />
-            <div style={{ height: '100vh'}}>
+            <div style={{ height: '100vh' }}>
                 <Calendar
                     dayLayoutAlgorithm={dayLayoutAlgorithm}
                     defaultDate={defaultDate}
